a1ee8f7990dff3a891c0a11f4df495eba1d6664a
Rodrigo Castro <rcastro@programmer.net>
2021-09-05 03:57:28 +0000

    Agregado proyecto de Spring Boot inicial

---
+++ src/main/java/com/devtools/mvnpipelinepoc/MvnPipelinePocApplication.java
@@ -0,0 +1,13 @@
+package com.devtools.mvnpipelinepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnPipelinePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnPipelinePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtools/mvnpipelinepoc/MvnPipelinePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtools.mvnpipelinepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnPipelinePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
